5304dc53c279cc834b4a87544fb804a4d9b8c298
tumon <tumonkhn@gmail.com>
2022-09-04 21:08:35 +0600

    init

--- src/components/header/dls/Btn.tsx
+++ src/components/header/dls/Btn.tsx
@@ -1,6 +1,6 @@
 import styles from "./Btn.module.scss";
 
-const Btn = ({ children }: { children: string }) => {
-  return <div className={styles.btn}> {children} </div>;
+const Btn = ({ hero, children }: { hero?: boolean; children: string }) => {
+  return <div className={hero ? styles.btnHero : styles.btn}> {children} </div>;
 };
 export default Btn;
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import Btn from "../header/dls/Btn";
 import styles from "./Hero.module.scss";
 const Hero = () => {
   return (
     <div className={styles.main}>
-      <div>Hello</div>
+      <div className={styles.titleDiv}>
+        <h1>Hypersonic Internet</h1>
+        <h1> Services Ltd.</h1>
+        <p>
+          {`Best internet service provider in Bangladesh which 
+          provides fully dedicated, super fast, cost-effective,
+          secured internet connection. We are promised to meeting your 
+          needs and delivering industry-leading customer service.`}
+        </p>
+
+        <div>
+          <Btn hero> Get Started </Btn>
+        </div>
+      </div>
 
       <div className={styles.imgHold}>
         <Image
