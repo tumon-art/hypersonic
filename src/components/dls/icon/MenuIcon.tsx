77be91ca16d2f47f6e86383c3b19465ac8f5520c
tumon <tumonkhn@gmail.com>
2022-09-06 03:36:56 +0600

    init

--- src/components/dls/icon/MenuIcon.tsx
+++ src/components/dls/icon/MenuIcon.tsx
@@ -4,23 +4,22 @@ import styles from "./MenuIcon.module.scss";
 const MenuIcon = () => {
   const { setSidebar } = useStore();
   return (
-    <div onClick={setSidebar}>
-      <svg
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className={styles.menuSvg}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75
+    <svg
+      onClick={setSidebar}
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={styles.menuSvg}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75
         5.25h16.5m-16.5 4.5h16.5m-16.5
         4.5h16.5m-16.5 4.5h16.5"
-        />
-      </svg>
-    </div>
+      />
+    </svg>
   );
 };
 export default MenuIcon;
