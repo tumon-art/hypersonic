095b9652ae7afa1b3e8abe169bede6dd96492931
tumon <tumonkhn@gmail.com>
2022-09-08 04:11:09 +0600

    init

--- src/components/pricing/Pricing.tsx
+++ src/components/pricing/Pricing.tsx
@@ -4,7 +4,23 @@ import styles from "./Pricing.module.scss";
 const Pricing = () => {
   return (
     <section id="pricing" className={styles.main}>
-      <div></div>
+      <div className={styles.cardHold}>
+        {pricingData.map<JSX.Element>((each) => {
+          return (
+            <div className={styles.card} key={each.id}>
+              <h3> {each.speed} </h3>
+
+              {/* === Price */}
+              <div>
+                <h1>
+                  {each.price}
+                  <span> / month</span>
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
