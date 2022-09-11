237151ffb16675ca0d388fac101c7b0fb7e4ac78
tumon <tumonkhn@gmail.com>
2022-09-11 15:48:05 +0600

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Navbar from "../components/header/Navbar";
 import Hero from "../components/hero/Hero";
-import useStore from "../store/mainStore";
 import FeaturedCard from "../components/featured/FeaturedComp";
 import About from "../components/about/About";
 import Pricing from "../components/pricing/Pricing";
 import Footer from "../components/footer/Footer";
 
 const Home: NextPage = () => {
-  const { sidebar } = useStore();
   return (
     <div>
       <Head>
@@ -19,22 +17,24 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
+      <section className={styles.section}>
+        <Navbar />
 
-      <section id="main">
-        <Hero />
+        <section id="main">
+          <Hero />
+        </section>
+
+        {/* ======= Featured Services Section =======  */}
+        <section className={styles.featuredSect}>
+          <FeaturedCard />
+        </section>
+
+        {/* ======= About Section =======  */}
+        <About />
+
+        {/* ======= Pricing Section =======  */}
+        <Pricing />
       </section>
-
-      {/* ======= Featured Services Section =======  */}
-      <section className={styles.featuredSect}>
-        <FeaturedCard />
-      </section>
-
-      {/* ======= About Section =======  */}
-      <About />
-
-      {/* ======= Pricing Section =======  */}
-      <Pricing />
 
       {/* ======= Footer Section =======  */}
       <Footer />
