f884ef97a3eb1e614c6cfd26d08b2cbfb750bece
tumon <tumonkhn@gmail.com>
2022-09-12 01:07:01 +0600

    init

---
+++ src/components/partners/Partners.tsx
@@ -0,0 +1,10 @@
+import styles from "./Partners.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+
+const Partners = () => {
+  return <div></div>;
+};
+export default Partners;
