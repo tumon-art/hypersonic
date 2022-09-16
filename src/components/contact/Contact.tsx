73e3428396b7db72c0147c9f60b0953171280eb2
tumon <tumonkhn@gmail.com>
2022-09-16 21:41:09 +0600

    init

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,23 @@
+import Btn from "../dls/btn/Btn";
 import styles from "./Contact.module.scss";
 
 const Contact = () => {
-  return <div>Contact us</div>;
+  return (
+    <section className={styles.section}>
+      <Btn nav> Contact </Btn>
+      <h1 className={styles.mainH1}>Contact Us</h1>
+      <p className={styles.mainP}>
+        Ut possimus qui ut temporibus culpa velit eveniet modi omnis est
+        adipisci expedita at voluptas atque vitae autem.
+      </p>
+
+      <div>
+        {/* === Address === */}
+        <div></div>
+        {/* === EMAIL & CALL */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 export default Contact;
